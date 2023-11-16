31dbe7e5ca8158e096ff37fafe6359b884529096
Albert Dedushi <adedushi@hotmail.com>
2023-11-16 15:23:44 -0500

    Complete associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    has_many :enrollments
+
+    has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
+
+    belongs_to :prerequisite,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true
+
+    belongs_to :instructor,
+    class_name: :User
 end
